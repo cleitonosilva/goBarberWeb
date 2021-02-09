bf08eeb050133509e65063e7c8be589b8a440d26
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-02-09 13:01:27 -0300

    Estilo dos components

--- src/pages/Signin/styles.ts
+++ src/pages/Signin/styles.ts
@@ -41,23 +41,7 @@ export const Content = styled.div`
         margin-top: 8px;
       }
     }
-    button {
-      background: #ff9000;
-      height: 56px;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      width: 100%;
-      color: #312e38;
-      font-weight: 500;
-      margin-top: 16px;
-      transition: background-color 0.3s;
-
-      &:hover{
-        background:${shade(0.2,'#ff9000' )};
-        color: #fff
-      }
-    }
+   
 
     a{  
       color:#f4ede8;
