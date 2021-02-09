39f223208b7b74f2029941edb730906d427fb941
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-02-09 13:01:05 -0300

    Importando components e passando parametros

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import logoimg from '../../assets/logo.svg';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import Input from '../../components/Input'
+import Button from '../../components/Button'
 
 import {Container, Content, Background } from './styles'
 
@@ -15,10 +18,10 @@ const Sigin: React.FC=()=> (
     <form>
         <h1>Faça seu Logon</h1>
 
-        <input placeholder="E-mail"/>
-        <input type="passowrd" placeholder="Senha"/>
+        <Input name="e-mail" icon={FiMail} placeholder="E-mail"/>
+        <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
     
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
    </form>
