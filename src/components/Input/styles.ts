7cf983050d0771484e481313c4f66eba8cbb3678
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-02-09 15:00:55 -0300

    estilos com linha no bluer

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,13 +1,18 @@
 
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
 
-export const Container =styled.div`
+export const Container =styled.div<ContainerProps>`
       background: #232129;
       border-radius: 10px;
-      border: 2px solid #232129;
-      
       width: 100%;
+
       color: #666360;
+      border: 2px solid #232129;
+
 
       display: flex;
       align-items: center;
@@ -15,6 +20,16 @@ export const Container =styled.div`
       & + div {
         margin-top: 8px;
       }
+
+${props => props.isFocused && css`
+      color: #ff9000;
+      border: 2px solid #ff9000;
+`  }
+
+${props => props.isFilled && css`
+      color: #ff9000;
+      
+`  }
 
 input {
     flex: 1;
