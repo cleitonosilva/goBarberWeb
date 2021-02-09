61cc957c9330467bb3fcc3f2ba32f654687ba7ae
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-02-09 15:00:17 -0300

    sem rota mudando full

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
-import Sigin from '../src/pages/Signin'
+import Sigin from '../src/pages/Signin';
+import SignUp from '../src/pages/Signup';
 
 const App: React.FC =() => 
 <>
-<Sigin/>
+<SignUp/>
 <GlobalStyle/>
 </>
 
