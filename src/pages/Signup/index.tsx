3771f78493bdd1794e17ab418b2fe754c074fe1b
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-02-09 15:01:11 -0300

    validando formulario

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -2,21 +2,29 @@ import React from 'react';
 
 import logoimg from '../../assets/logo.svg';
 import {  FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
 import {Container, Content, Background } from './styles'
 
-const SignUp: React.FC=()=> (
+const SignUp: React.FC=()=> {
 
+function handleSubmit(data: object) :  void{
+    console.log(data);
+}
+
+
+
+return (
 <>
 <Container>
     <Background />
     <Content>
     <img src={logoimg} alt="GoBarber"/>
 
-    <form>
+    <Form onSubmit={handleSubmit}>
         <h1>Faça seu Cadastro</h1>
 
         <Input name="name" icon={FiUser} placeholder="Nome"/>
@@ -25,11 +33,18 @@ const SignUp: React.FC=()=> (
     
         <Button type="submit">Cadastrar</Button>
 
-   </form>
+   </Form>
        <a href="login"> <FiArrowLeft/>Voltar para Logon</a>
     </Content>
 </Container>
 </>
-);
+
+
+
+)
+
+
+}
+
 
 export default SignUp;
